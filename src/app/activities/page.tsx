7750eff83aa49a4b553a9c7c94827e72de1a296e
unknown <syabdoul100@gmail.com>
2023-10-23 09:38:00 +0000

    Deleting top level App directory added by shadcn ui

--- src/app/activities/page.tsx
+++ src/app/activities/page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <div className="mx-auto font-bold text-xl uppercase text-slate-600 mt-10">
+      Activities page
+    </div>
+  );
 }
